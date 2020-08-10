0ae2624be2b90b763c9c1de48b9aa65418b2383b
manoopratap@gmail.comcd <manoopratap@gmail.com>
2020-08-10 13:05:49 +0530

    updateded aws jenkins

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 
     return (
       <div className="App-header">
-        <div>Mannoo Pratap</div>
+        <div>Mannoo Pratap Singh</div>
       <WorldMap/>
       <BubbleCircleChart/>
       <FreeCodeCampBarChart/>
