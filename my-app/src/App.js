1f378a62b5bd8fb7d4b9ee83e888640988603e90
manoopratap <40164473+manoopratap@users.noreply.github.com>
2020-08-15 23:28:49 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 
     return (
       <div className="App-header">
-        <div>Mannoo Pratap</div>
+        <div>Mannoo Pratap singh</div>
       <WorldMap/>
       <BubbleCircleChart/>
       <FreeCodeCampBarChart/>
